1f3ed69880d58d9e29cd673d6f0ad6f8013fb5d5
Alina <62746485+Alinoka-boop@users.noreply.github.com>
2020-05-21 22:42:53 +0300

    Derevo

---
+++ Lab8/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BST;
+//В данном решении объединено первое и второе задание 
+//В Tree.cs представлены основные операции с деревьями 
+//ссылка на гитхаб https://github.com/Alinoka-boop/ASiD
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+            int[] elems = { 20, 5, 40, 50 };//исходные данные 
+            for (int i = 0; i < elems.Length; i++)
+            {
+                tree.Push(elems[i]);
+            }
+
+            tree.Print();
+
+            Tree[] leafs = tree.GetLeafs(elems);
+            int[] mTRN = { };                               
+            int[] lTRN = { };                               
+            int[] lVal = { }, rVal = { };
+            int[,] deep = new int[leafs.Length, 2];
+            int[,] rootDeep = new int[leafs.Length, 2];
+            int rootLength = 0;
+            int maxL = -1, maxR = -1;
+            int wayLength;
+
+            Tree root = tree.Find(elems[0]);
+            int rootNode = elems[0];
+
+            if (root.left == null || root.right == null)
+            {
+                for (int i = 0; i < leafs.Length; i++)
+                {
+                    rootDeep[i, 0] = (int)leafs[i].value;
+                    rootDeep[i, 1] = Tree.Deep(leafs[i]) + 1;
+                }
+
+                for (int i = 0; i < rootDeep.GetLength(0); i++)
+                {
+                    if (rootLength < rootDeep[i, 1])
+                    {
+                        rootLength = rootDeep[i, 1];
+                    }
+                }
+
+                while (!((root.left != null && root.right != null) || (root.left == null && root.right == null)))
+                {
+                    if (root.left != null)
+                    {
+                        root = root.left;
+                        rootNode = (int)root.value;
+                        continue;
+                    }
+
+                    root = root.right;
+                    rootNode = (int)root.value;
+                }
+            }
+
+            for (int i = 0; i < leafs.Length; i++)
+            {
+                deep[i, 0] = (int)leafs[i].value;
+                deep[i, 1] = Tree.Deep(leafs[i]);
+
+                if (deep[i, 0] > rootNode)
+                {
+                    Array.Resize(ref mTRN, mTRN.Length + 1);
+                    Array.Resize(ref rVal, rVal.Length + 1);
+                    mTRN[mTRN.Length - 1] = deep[i, 0];
+                    rVal[rVal.Length - 1] = deep[i, 1];
+                    if (maxR < rVal[rVal.Length - 1])
+                    {
+                        maxR = rVal[rVal.Length - 1];
+                    }
+                }
+                else
+                {
+                    Array.Resize(ref lTRN, lTRN.Length + 1);
+                    Array.Resize(ref lVal, lVal.Length + 1);
+                    lTRN[lTRN.Length - 1] = deep[i, 0];
+                    lVal[lVal.Length - 1] = deep[i, 1];
+                    if (maxL < lVal[lVal.Length - 1])
+                    {
+                        maxL = lVal[lVal.Length - 1];
+                    }
+                }
+            }
+
+            wayLength = maxL + maxR - 1;
+            int[] wayNodes = { };
+
+            if (wayLength < rootLength)
+            {
+                for (int i = 0; i < rootDeep.GetLength(0); i++)
+                {
+                    if (rootDeep[i, 1] == rootLength)
+                    {
+                        int[] nodes = tree.GetNodes(rootDeep[i, 0], elems[0]);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+            }
+            else if (wayLength == rootLength)
+            {
+                for (int i = 0; i < rootDeep.GetLength(0); i++)
+                {
+                    if (rootDeep[i, 1] == rootLength)
+                    {
+                        int[] nodes = tree.GetNodes(rootDeep[i, 0], elems[0]);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < mTRN.Length; i++)
+                {
+                    if (rVal[i] == maxR)
+                    {
+                        int[] nodes = tree.GetNodes(mTRN[i], rootNode);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < lTRN.Length; i++)
+                {
+                    if (lVal[i] == maxL)
+                    {
+                        int[] nodes = tree.GetNodes(lTRN[i], rootNode);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < mTRN.Length; i++)
+                {
+                    if (rVal[i] == maxR)
+                    {
+                        int[] nodes = tree.GetNodes(mTRN[i], rootNode);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+
+                for (int i = 0; i < lTRN.Length; i++)
+                {
+                    if (lVal[i] == maxL)
+                    {
+                        int[] nodes = tree.GetNodes(lTRN[i], rootNode);
+                        Array.Resize(ref wayNodes, wayNodes.Length + nodes.Length);
+                        for (int j = wayNodes.Length - nodes.Length, k = 0; j < wayNodes.Length; j++, k++)
+                        {
+                            wayNodes[j] = nodes[k];
+                        }
+                    }
+                }
+            }
+
+            Array.Sort(wayNodes);
+            wayNodes = wayNodes.Distinct().ToArray();
+
+            foreach (int i in wayNodes)
+                Console.Write(i + " ");
+
+            if (wayNodes.Length < 2)
+            {
+                Console.WriteLine("Невозможно удалить, так как элементов в пути меньше 2-ух");
+            }
+            else
+            {
+                tree.DeleteRight(wayNodes[1]);
+            }
+
+            tree.Print();
+
+            Console.WriteLine(Tree.CLR(tree));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab8/Tree.cs
@@ -0,0 +1,562 @@
+﻿using System;
+
+namespace BST
+{
+    public class Tree
+    {
+        public int? value { get; private set; }
+        public Tree left;
+        public Tree right;
+        public Tree parent;
+        public int nodeCount;
+        private int? rootValue = null;
+
+        public int NodeCount
+        {
+            get
+            {
+                if (parent == null)
+                {
+                    return nodeCount;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+
+        public Tree()
+        {
+            value = null;
+            left = null;
+            right = null;
+            parent = null;
+            nodeCount = 0;
+        }
+
+        public void Push(int elem)
+        {
+            if (value == null || value == elem)
+            {
+                if (value == elem)
+                {
+                    nodeCount--;
+                }
+
+                value = elem;
+            }
+
+            if (value > elem)
+            {
+                if (left == null)
+                {
+                    left = new Tree();
+                }
+                Push(elem, left, this);
+            }
+
+            if (value < elem)
+            {
+                if (right == null)
+                {
+                    right = new Tree();
+                }
+                Push(elem, right, this);
+            }
+
+            nodeCount++;
+        }
+
+        private void Push(int elem, Tree node, Tree parent)
+        {
+            if (node.value == null || node.value == elem)
+            {
+                node.value = elem;
+                node.parent = parent;
+                return;
+            }
+
+            if (node.value > elem)
+            {
+                if (node.left == null)
+                {
+                    node.left = new Tree();
+                }
+                Push(elem, node.left, node);
+            }
+
+            if (node.value < elem)
+            {
+                if (node.right == null)
+                {
+                    node.right = new Tree();
+                }
+                Push(elem, node.right, node);
+            }
+        }
+
+        public Tree Find(int elem)
+        {
+            Tree parent = this;
+            while (parent.parent != null)
+            {
+                parent = parent.parent;
+            }
+
+            if (parent.value == elem)
+            {
+                return this;
+            }
+
+            if (parent.value > elem)
+            {
+                if (left == null)
+                {
+                    return null;
+                }
+                return Find(elem, parent.left);
+            }
+
+            if (value < elem)
+            {
+                if (right == null)
+                {
+                    return null;
+                }
+                return Find(elem, parent.right);
+            }
+
+            return null;
+        }
+
+        private Tree Find(int elem, Tree node)
+        {
+            if (node.value == elem)
+            {
+                return node;
+            }
+
+            if (node.value > elem)
+            {
+                if (node.left == null)
+                {
+                    return null;
+                }
+
+                return Find(elem, node.left);
+            }
+
+            if (node.value < elem)
+            {
+                if (node.right == null)
+                {
+                    return null;
+                }
+                return Find(elem, node.right);
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            if (left == null && right != null)
+            {
+                return String.Format("{0} <- {1} -> {2}", "null", value, right.value);
+            }
+            else if (left != null && right == null)
+            {
+                return String.Format("{0} <- {1} -> {2}", left.value, value, "null");
+            }
+            else if (left == null && right == null)
+            {
+                return String.Format("{0} <- {1} -> {2}", "null", value, "null");
+            }
+            else
+            {
+                return String.Format("{0} <- {1} -> {2}", left.value, value, right.value);
+            }
+        }
+
+        public bool DeleteRight(int elem)
+        {
+            Tree treeElem = Find(elem);
+            if (treeElem == null)
+            {
+                return false;
+            }
+
+            return DelRght(treeElem);
+        }
+
+        private bool DelRght(Tree elem)
+        {
+            if (elem.left == null && elem.right == null)
+            {
+                if (elem.parent.left == elem)
+                {
+                    elem.parent.left = null;
+                }
+                else
+                {
+                    elem.parent.right = null;
+                }
+
+                return true;
+            }
+            else if (elem.left != null && elem.right == null)
+            {
+                elem.left.parent = elem.parent;
+                elem.parent.left = elem.left;
+
+                return true;
+            }
+            else if (elem.left == null && elem.right != null)
+            {
+                elem.right.parent = elem.parent;
+                elem.parent.right = elem.right;
+
+                return true;
+            }
+            else
+            {
+                Tree newElem = elem.right;
+
+                while (newElem.left != null)
+                {
+                    newElem = newElem.left;
+                }
+
+                DeleteRight((int)newElem.value);
+
+                elem.value = newElem.value;
+
+                return true;
+            }
+        }
+
+        public bool DeleteLeft(int elem)
+        {
+            Tree treeElem = Find(elem);
+            if (treeElem == null)
+            {
+                return false;
+            }
+
+            return DelLeft(treeElem);
+        }
+
+        private bool DelLeft(Tree elem)
+        {
+            if (elem.left == null && elem.right == null)
+            {
+                if (elem.parent.left == elem)
+                {
+                    elem.parent.left = null;
+                }
+                else
+                {
+                    elem.parent.right = null;
+                }
+
+                return true;
+            }
+            else if (elem.left != null && elem.right == null)
+            {
+                elem.left.parent = elem.parent;
+                elem.parent.left = elem.left;
+
+                return true;
+            }
+            else if (elem.left == null && elem.right != null)
+            {
+                elem.right.parent = elem.parent;
+                elem.parent.right = elem.right;
+
+                return true;
+            }
+            else
+            {
+                Tree newElem = elem.left;
+
+                while (newElem.right != null)
+                {
+                    newElem = newElem.right;
+                }
+
+                DeleteRight((int)newElem.value);
+
+                elem.value = newElem.value;
+
+                return true;
+            }
+        }
+
+        static public string CLR(Tree elem)
+        {
+            string path = "";
+
+            path = elem.value.ToString();
+
+            if (elem.left == null && elem.right == null)
+            {
+                return path;
+            }
+            else
+            {
+                if (elem.left != null)
+                {
+                    path += " " + CLR(elem.left);
+                }
+                if (elem.right != null)
+                {
+                    path += " " + CLR(elem.right);
+                }
+
+                return path;
+            }
+        }
+
+        static public string LRC(Tree elem)
+        {
+            string path = "";
+
+            if (elem.left == null && elem.right == null)
+            {
+                return elem.value.ToString();
+            }
+            else
+            {
+                if (elem.left != null)
+                {
+                    path += LRC(elem.left) + " ";
+                }
+                if (elem.right != null)
+                {
+                    path += LRC(elem.right) + " ";
+                }
+
+                path += elem.value.ToString();
+                return path;
+            }
+        }
+
+        static public string LCR(Tree elem)
+        {
+            string path = "";
+
+            if (elem.left == null && elem.right == null)
+            {
+                return elem.value.ToString();
+            }
+            else
+            {
+                if (elem.left != null)
+                {
+                    path += LCR(elem.left) + " ";
+                }
+
+                path += elem.value.ToString() + " ";
+
+                if (elem.right != null)
+                {
+                    path += LCR(elem.right) + " ";
+                }
+
+                return path;
+            }
+        }
+
+        static public int Deep(Tree elem)
+        {
+            int deep = 0;
+
+            while (elem.parent != null)
+            {
+                deep++;
+                elem = elem.parent;
+            }
+
+            return deep;
+        }
+
+        static public int Height(Tree elem)
+        {
+            int height = 1;
+
+            while (elem.left != null)
+            {
+                height++;
+                elem = elem.left;
+            }
+
+            while (elem.right != null)
+            {
+                height++;
+                elem = elem.right;
+            }
+
+            return height;
+        }
+
+        static public int Level(Tree elem)
+        {
+            int deep = Deep(elem);
+
+            while (elem.parent != null)
+            {
+                elem = elem.parent;
+            }
+
+            return Height(elem) - deep;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Tree node = this;
+            while (node.parent != null)
+            {
+                node = node.parent;
+            }
+
+            rootValue = node.value;
+
+            Console.Write("\u2514 ");
+            Prnt(node);
+            Console.WriteLine();
+        }
+
+        private void Prnt(Tree elem)
+        {
+            int deep = Deep(elem);
+
+            Console.WriteLine(elem.value);
+
+            //Console.WriteLine(" Deep = " + deep);
+
+            Console.Write("  ");
+            if (elem.left != null)
+            {
+                for (int i = 0; i < deep; i++)
+                {
+                    if (elem.left.value < rootValue)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.Write("\u2502    ");
+                        }
+                        else
+                        {
+                            Console.Write("     ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("     ");
+                    }
+                }
+                Console.Write("\u251C L: ");
+                Prnt(elem.left);
+            }
+            else if (elem.left == null && elem.right != null)
+            {
+                for (int i = 0; i < deep; i++)
+                {
+                    if (elem.right.value < rootValue)
+                    {
+                        Console.Write("\u2502    ");
+                    }
+                    else
+                    {
+                        Console.Write("     ");
+                    }
+                }
+                Console.Write("\u251C L: ");
+                Console.WriteLine("-");
+                Console.Write("  ");
+            }
+
+            if (elem.right != null)
+            {
+                for (int i = 0; i < deep; i++)
+                {
+                    if (elem.right.value < rootValue)
+                    {
+                        if (i % 2 == 0)
+                        {
+                            Console.Write("\u2502    ");
+                        }
+                        else
+                        {
+                            Console.Write("     ");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("     ");
+                    }
+                }
+
+                Console.Write("\u2514 R: ");
+                Prnt(elem.right);
+            }
+            else if (elem.left != null && elem.right == null)
+            {
+                for (int i = 0; i < deep; i++)
+                {
+                    if (elem.left.value < rootValue)
+                    {
+                        Console.Write("\u2502    ");
+                    }
+                    else
+                    {
+                        Console.Write("     ");
+                    }
+                }
+                Console.Write("\u2514 R: ");
+
+                Console.WriteLine("-");
+                Console.Write("  ");
+            }
+        }
+
+        public Tree[] GetLeafs(int[] elems)
+        {
+            Tree[] leafs = { };
+
+            for (int i = 0; i < elems.Length; i++)
+            {
+                Tree node = Find(elems[i]);
+
+                if (node != null && node.left == null && node.right == null)
+                {
+                    Array.Resize(ref leafs, leafs.Length + 1);
+                    leafs[leafs.Length - 1] = node;
+                }
+            }
+
+            return leafs;
+        }
+
+        public int[] GetNodes(int from, int rootNode)
+        {
+            Tree root = Find(rootNode);
+            int[] nodes = { };
+
+            Tree node = Find(from);
+            while (node.parent != root.parent)
+            {
+                Array.Resize(ref nodes, nodes.Length + 1);
+                nodes[nodes.Length - 1] = (int)node.value;
+                node = node.parent;
+            }
+
+            Array.Resize(ref nodes, nodes.Length + 1);
+            nodes[nodes.Length - 1] = (int)node.value;
+
+            return nodes;
+        }
+    }
+}
